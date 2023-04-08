bedc040a76e8aea1a677a47ad335112fea319df9
Duy Anh <aduy644@gmail.com>
2023-04-09 01:17:55 +0700

    ins

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const hbs  = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+const route = require("./routes");
 
-const route = require('./routes');
-
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
-// XMLHttpRequest, fetch, axios, 
+// XMLHttpRequest, fetch, axios,
 // app.use(morgan('combined'));
 
 //Teamplate engine
-app.engine('hbs', hbs.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 // console.log(__dirname)
 // console.log('PATH: ', path.join(__dirname, 'resources/views') )
 
 // Route init
 route(app);
 
-
-
-
 // 127.0.0.1 -localhost
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const siteController = require('../app/controllers/SiteController')
+const siteController = require("../app/controllers/SiteController");
 
-router.use('/search', newsController.search)
-router.use('/', newsController.index)
-
+router.use("/search", newsController.search);
+router.use("/", newsController.index);
 
 module.exports = router;
